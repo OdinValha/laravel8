d7562f5db7adc5ebd281204bbdc3b0b64f71909a
Alanda0305 <pichet.mek@vru.ac.th>
2023-12-04 11:47:05 +0700

    week 4

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\MyProfileController;
+use App\Http\Controllers\QuizController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,31 +36,31 @@ Route::get('/homepage', function () {
     return "<h1>This is home page !!! </h1>";
 });
 
-Route::get("/blog/{id}", function($id) {
-    return "<h1>This is blog page : {$id} </h1>" ;
+Route::get("/blog/{id}", function ($id) {
+    return "<h1>This is blog page : {$id} </h1>";
 });
 
-Route::get("/hello", function () {	
+Route::get("/hello", function () {
     return view("hello");
-    });
+});
 
 Route::get('/greeting', function () {
 
-	$name = 'James';
-$last_name = 'Mars';
+    $name = 'James';
+    $last_name = 'Mars';
 
-return view('greeting', compact('name','last_name') );
+    return view('greeting', compact('name', 'last_name'));
 });
 
 
-Route::get( "/gallery" , function(){
-$ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-$bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg"; 
-$cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
-$god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg"; 
-$spider = "https://blog.playstation.com/tachyon/2022/06/0c3c20a8d8514501524a0859461f391572ea6e61.jpg?resize=1088%2C612&crop_strategy=smart"; 
+Route::get("/gallery", function () {
+    $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
+    $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg";
+    $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
+    $god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg";
+    $spider = "https://blog.playstation.com/tachyon/2022/06/0c3c20a8d8514501524a0859461f391572ea6e61.jpg?resize=1088%2C612&crop_strategy=smart";
 
-return view("test/index", compact("ant","bird","cat","god","spider") );
+    return view("test/index", compact("ant", "bird", "cat", "god", "spider"));
 });
 
 
@@ -79,16 +82,16 @@ Route::get("/gallery/cat", function () {
 
 // week 2
 
-Route::get("/teacher" , function (){
-	return view("teacher");
+Route::get("/teacher", function () {
+    return view("teacher");
 });
 
-Route::get("/student" , function (){
-	return view("student");
+Route::get("/student", function () {
+    return view("student");
 });
 
-Route::get("/theme" , function (){
-	return view("theme");
+Route::get("/theme", function () {
+    return view("theme");
 });
 
 // Route Template Inheritance
@@ -114,3 +117,49 @@ Route::get('/tables', function () {
 Route::get('/component', function () {
     return view('SB Admin 2 - Tables-component');
 });
+
+// week3
+Route::get("/myprofile/create", [MyProfileController::class, "create"]);
+
+Route::get("/myprofile/{id}/edit", [MyProfileController::class, "edit"]);
+
+Route::get("/myprofile/{id}", [MyProfileController::class, "show"]);
+
+Route::get("/coronavirus", [MyProfileController::class, "coronavirus"]);
+
+// quiz
+
+// Route::get('study-question', function () {
+//     $questions = json_decode(file_get_contents("https://raw.githubusercontent.com/arc6828/cs/master/json/sci-mbti.json"));
+//     return view("study/question", compact('questions'));
+// })->name('study-question');
+
+// Route::post('study-match', function (Request $request) {
+//     $summary = ["CS" => 0, "IT" => 0, "DISE" => 0, "HE" => 0, "NU" => 0, "FB" => 0, "SET" => 0, "OHS" => 0];
+//     $majors = [
+//         "CS" => "วิทยาการคอมพิวเตอร์ (CS)",
+//         "IT" => "เทคโนโลยีสารสนเทศ (IT)",
+//         "DISE" => "นวัตกรรมดิจิทัลและวิศวกรรมซอฟต์แวร์ (DISE)",
+//         "HE" => "คหกรรมศาสตร์ (HE)",
+//         "NU" => "โภชนาการและการกำหนดอาหาร (NU)",
+//         "FB" => "นวัตกรรมอาหารและเครื่องดื่มเพื่อสุขภาพ (FB)",
+//         "SET" => "วิทยาศาสตร์และเทคโนโลยีสิ่งแวดล้อม (SET)",
+//         "OHS" => "อาชีวอนามัยและความปลอดภัย (OHS)",
+//     ];
+//     foreach ($_POST as $key => $value) {
+//         if (!str_contains($key, "flexRadioDefault")) continue;
+//         [$code, $answer] = explode("-", $value);
+//         if ($answer == "yes") {            // if-yes
+//             $summary[$code] = isset($summary[$code]) ? $summary[$code] + 1 : 1;
+//         } else {            // if-no
+//             $summary[$code] = isset($summary[$code]) ? $summary[$code] + 0 : 0;
+//         }
+//     }
+//     $codes = array_keys($summary);
+//     $values = array_values($summary);
+
+//     return view("study/match", compact('codes', 'values', 'majors'));
+// })->name('study-match');
+
+Route::get("study-question", [ QuizController::class, "question" ])->name("study-question");
+Route::post("study-match", [ QuizController::class, "match" ])->name("study-match");
